6438cffdb1bc0ff5b13369d2b56791829b7c83d7
Ian Kenney <ian.kenney@asu.edu>
2018-04-08 12:18:14 -0700

    Added scipy as a dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name="multibind",
       version="0.1.0",
-      description="A library for calcuating effective binding free
-      energies of systems with multiple protonation states.",
+      description="A library for calcuating effective free energies of systems with multiple protonation states.",
       author="Ian Kenney",
       author_email="ian.kenney@asu.edu",
       packages=find_packages(),
-      install_requires=['numpy', 'networkx', 'pandas', 'matplotlib'],
+      install_requires=['numpy', 'networkx', 'pandas', 'matplotlib','scipy'],
       tests_require=['pytest']
 )
