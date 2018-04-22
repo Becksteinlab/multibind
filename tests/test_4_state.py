ca6d4c998ed52f494ba4dd92b7029c30a008d340
Ian Kenney <ian.kenney@asu.edu>
2018-04-21 18:07:08 -0700

    basic testing

---
+++ tests/test_4_state.py
@@ -0,0 +1,30 @@
+import pytest
+import numpy.testing as npt
+import multibind as mb
+import numpy as np
+
+class TestG(object):
+
+    def setup(self):
+        self.c = mb.Multibind()
+        self.c.read_graph("../examples/input/4-state-diamond/graph.csv",comment="#")
+        self.c.read_states("../examples/input/4-state-diamond/states.csv")
+
+
+    def test_length(self):
+        """Does the algorithm give us the right number of results..."""
+        self.c.build_cycle()
+        self.c.MLE()
+        assert len(self.c.g_mle) == 4
+
+    def test_pH_5(self):
+        self.c.build_cycle(pH=5)
+        self.c.MLE()
+        # Found from maxima script
+        # mb will output this order because of the state file order [g1, g2, g4, g3]
+        g_analytic = np.array([4.237564495703078, 1.987494501321176,
+                               0.0, -2.35510019160619])
+        g_analytic = g_analytic - g_analytic[0]
+        
+        npt.assert_almost_equal(self.c.g_mle,
+                                g_analytic)
