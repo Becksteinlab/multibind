a3d97cd7f3637f21a573e7129cf20b1e7c82a5ab
Ian Kenney <ian.kenney@asu.edu>
2018-05-11 11:09:32 -0700

    Fixed __init__.py for multibind

--- multibind/__init__.py
+++ multibind/__init__.py
@@ -1,3 +1,3 @@
-from multibind.effective_binding_energies import *
-from multibind.multibind import Multibind
-from multibind.multibind import MultibindDriver
+from .effective_binding_energies import *
+from .multibind import Multibind
+from .multibind import MultibindDriver
