242093392e7c7860fdbf560bd6252ae64bf5fdcc
Ian Kenney <ikenney@asu.edu>
2021-01-18 17:30:40 -0700

    Unused imports

--- driver.py
+++ driver.py
@@ -1,9 +1,7 @@
 import multibind as mb
-import matplotlib.pyplot as plt
-import networkx as nx
 
 c = mb.Multibind()
-c.read_graph("examples/input/4-state-diamond/graph.csv",comment="#")
+c.read_graph("examples/input/4-state-diamond/graph.csv", comment="#")
 c.read_states("examples/input/4-state-diamond/states.csv")
 c.build_cycle()
 res = c.MLE()
